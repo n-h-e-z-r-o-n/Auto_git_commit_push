7e1eea0c5d7889b806c51db81dba2c0eb1e36e8a
Hezron Na <hezron.w12@gmail.com>
2023-12-22 10:43:52 +0300

    Committed 2023 1Fr.104352

---
+++ source_code/version 1/auto_git.bat
@@ -0,0 +1,4 @@
+cd C:/Users/HEZRON WEKESA/Desktop/python Project/Compiler Build/Compiler
+git add --all
+git commit -m "Committed %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+exit
