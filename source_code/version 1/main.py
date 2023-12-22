a019d989b21f32e6579a83be2ce0b8aa3190f158
Hezron Na <hezron.w12@gmail.com>
2023-12-22 13:49:31 +0300

    Committed 2023-12-22 13:49:31

--- source_code/version 1/main.py
+++ source_code/version 1/main.py
@@ -42,16 +42,16 @@ def validate_int(input):
 
 
 def git_comit_push():
-    global stop_process, result
+    global stop_process, result, PATH_ENTRY
     if stop_process != 'yes':
         global TERMINAL_WIDGET, git_commit_push_count, STATUS, app, app
         try:
             current_datetime = datetime.now()
             formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-            result_add = subprocess.run(["git", "add", "--all"], cwd= file_path, check=True)
-            result_commit =subprocess.run(["git", "commit", "-m", f"Committed {formatted_datetime}"], check=True)
-            result_push = subprocess.run(["git", "push"], check=True)
+            result_add = subprocess.run(["git", "add", "--all"], cwd= PATH_ENTRY.get(), check=True)
+            result_commit =subprocess.run(["git", "commit", "-m", f"Committed {formatted_datetime}"],cwd= PATH_ENTRY.get(), check=True)
+            result_push = subprocess.run(["git", "push"], cwd= PATH_ENTRY.get(), check=True)
 
             # Display output in Tkinter Text widget
             TERMINAL_WIDGET.insert(tk.END, f"git add:\n{result_add.stdout}\n\n")
