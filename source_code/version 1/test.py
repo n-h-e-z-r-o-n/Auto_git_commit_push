66d6a0ef07c5ad32a98b7898e512e9ec869f12a7
Hezron Na <hezron.w12@gmail.com>
2023-12-22 11:48:19 +0300

    Committed 2023 1Fr.114819

--- source_code/version 1/test.py
+++ source_code/version 1/test.py
@@ -1,1 +1,16 @@
 
+from datetime import datetime
+
+# Get current date and time
+current_datetime = datetime.now()
+
+# Format the current date and time as a string
+formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+print("Current Date and Time:", formatted_datetime)
+
+# If you want to get only the current date
+current_date = current_datetime.date()
+formatted_date = current_date.strftime("%Y-%m-%d")
+
+print("Current Date:", formatted_date)
