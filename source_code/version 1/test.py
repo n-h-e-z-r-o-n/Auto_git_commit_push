1a0fce3aa150aa780e99bdf100554e9479412494
Hezron Na <hezron.w12@gmail.com>
2023-12-22 13:57:39 +0300

    Committed 2023-12-22 13:57:39

--- source_code/version 1/test.py
+++ source_code/version 1/test.py
@@ -23,12 +23,13 @@ def has_changes():
 def git_commit_push():
     global stop_process, git_commit_push_count, STATUS, app
     if stop_process != 'yes':
+        git_directory = "C:\Users\HEZRON WEKESA\Desktop\python Project\git_Commit_App\Auto_git_commit_push"
         if has_changes():
             try:
                 # Redirect subprocess output
-                result_add = subprocess.run(["git", "add", "."], capture_output=True, text=True, check=True)
-                result_commit = subprocess.run(["git", "commit", "-m", "Your commit message"], capture_output=True, text=True, check=True)
-                result_push = subprocess.run(["git", "push"], capture_output=True, text=True, check=True)
+                result_add = subprocess.run(["git", "add", "--all"], cwd=git_directory, capture_output=True, text=True, check=True)
+                result_commit = subprocess.run(["git", "commit", "-m", "Your commit message"], cwd=git_directory, capture_output=True, text=True, check=True)
+                result_push = subprocess.run(["git", "push"], cwd=git_directory, capture_output=True, text=True, check=True)
 
                 # Display output in Tkinter Text widget
                 output_text.insert(tk.END, f"git add:\n{result_add.stdout}\n\n")
