59e70d0ab2d643aa5ec570c34e7d0775d84cb644
Hezron Na <hezron.w12@gmail.com>
2023-12-22 10:56:59 +0300

    Committed 2023 1Fr.105659

--- source_code/version 1/main.py
+++ source_code/version 1/main.py
@@ -92,6 +92,16 @@ def download_app_icon():
     with open(filename, 'wb') as f:
         f.write(response.content)
 
+def dark_title_bar(window):
+    window.update()
+    DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ct.windll.user32.GetParent
+    hwnd = get_parent(window.winfo_id())
+    rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+    value = 2
+    value = ct.c_int(value)
+    set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
 
 def main():
     global PATH_ENTRY, STATUS, TIME_INTERVAL, app, STATUS_2
