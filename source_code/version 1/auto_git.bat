6c134a25e37e0df4610dc09f22238c7d6b4a1cbf
Hezron Na <hezron.w12@gmail.com>
2023-12-22 15:40:41 +0300

    version 2

--- source_code/version 1/auto_git.bat
+++
@@ -1,4 +0,0 @@
-cd C:/Users/HEZRON WEKESA/Desktop/python Project/Compiler Build/Compiler
-git add --all
-git commit -m "Committed %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-exit
