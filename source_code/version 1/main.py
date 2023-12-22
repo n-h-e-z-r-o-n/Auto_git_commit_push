2835baed123d4af18f2e8687a1ce874a0c701b75
Hezron Na <hezron.w12@gmail.com>
2023-12-22 14:16:26 +0300

    Committed 2023 1Fr.141626

--- source_code/version 1/main.py
+++ source_code/version 1/main.py
@@ -193,7 +193,7 @@ def main():
     ABOUT.place(relx=0.01, rely=0.968, relheight=0.03, relwidth=0.12)
     change_fg_OnHover(ABOUT, '#2F4F4F', fg_color)
 
-    TERMINAL_WIDGET = tk.Text(app, bg='white', fg="gray", font=("Courier New", 8), borderwidth=0, border=0)
+    TERMINAL_WIDGET = tk.Text(app, bg=bg_color, fg="gray", font=("Courier New", 8), borderwidth=0, border=0)
     TERMINAL_WIDGET.place(relx=0.2, rely=0.4, relheight=0.3, relwidth=0.6)
 
 
