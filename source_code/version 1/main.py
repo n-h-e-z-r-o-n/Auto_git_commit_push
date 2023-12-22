920e72baa4d3e2c9c82dd89aa71bd5e6b63fbe21
Hezron Na <hezron.w12@gmail.com>
2023-12-22 11:45:08 +0300

    Committed 2023 1Fr.114508

--- source_code/version 1/main.py
+++ source_code/version 1/main.py
@@ -43,7 +43,7 @@ def git_comit_push():
     if stop_process != 'yes':
         global git_commit_push_count, STATUS, app, app
         subprocess.run(["git", "add", "--all"], check=True)
-        subprocess.run(["git", "commit", "-m", "Committed %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"], check=True)
+        subprocess.run(["git", "commit", "-m", "Committed %date:~-4%%date:~3,2%%date:~0,2%.% time:~0,2%%time:~3,2%%time:~6,2%"], check=True)
         subprocess.run(["git", "push"], check=True)
         #os.system(bat_file_c)
         STATUS.config(text=f'Committed and Pushed made: {git_commit_push_count} ')
