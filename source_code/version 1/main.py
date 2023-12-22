ec77edb975e443aa5394b9eb8922d5829b0684cd
Hezron Na <hezron.w12@gmail.com>
2023-12-22 13:45:00 +0300

    Committed 2023-12-22 13:45:00

--- source_code/version 1/main.py
+++ source_code/version 1/main.py
@@ -48,6 +48,7 @@ def git_comit_push():
         try:
             current_datetime = datetime.now()
             formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            subprocess.run([f"cd {file_path}\n"], check=True)
             result_add = subprocess.run(["git", "add", "--all"], check=True)
             result_commit =subprocess.run(["git", "commit", "-m", f"Committed {formatted_datetime}"], check=True)
             result_push = subprocess.run(["git", "push"], check=True)
