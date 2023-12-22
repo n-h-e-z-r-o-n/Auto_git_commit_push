7f07f817e96ab1e8767466e6ae176f50deecdbb2
Hezron Na <hezron.w12@gmail.com>
2023-12-22 12:44:01 +0300

    Committed 2023 1Fr.124401

--- source_code/version 1/main.py
+++ source_code/version 1/main.py
@@ -156,11 +156,11 @@ def main():
 
 
     START_BTN = tk.Button(app, text='START', bg='#F5F5F5', borderwidth=0, border=0, command=start, font=("Courier New", 11))
-    START_BTN.place(relx=0.4, rely=0.86, relheight=0.04, relwidth=0.12)
+    START_BTN.place(relx=0.4, rely=0.24, relheight=0.04, relwidth=0.12)
     change_bg_OnHover(START_BTN, '#EEEEFF', '#F5F5F5')
 
     STOP_BTN = tk.Button(app, text='STOP', bg='#F5F5F5', borderwidth=0, border=0, command=stop, font=("Courier New", 11))
-    STOP_BTN.place(relx=0.53, rely=0.86, relheight=0.04, relwidth=0.12)
+    STOP_BTN.place(relx=0.53, rely=0.24, relheight=0.04, relwidth=0.12)
     change_bg_OnHover(STOP_BTN, '#EEEEFF', '#F5F5F5')
 
     SUPPORT = tk.Button(app, text='support', bg=bg_color, activebackground=bg_color, fg="gray", activeforeground='red',  font=("Courier New italic", 8), borderwidth=0, border=0, command=support_info)
